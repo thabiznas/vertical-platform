9564862ab991c6b5f68b14e3693af514bdcaf617
Hunter Stevens <hunter@toplobster.io>
2019-01-17 17:00:24 +0000

    Added teachers pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,9 +14,23 @@ end
 
 def join
 end
+
+def enterprise
+end
   
+def help
+end
+
+def teacher_jorge_carrasco
+end
+
+def teacher_mya_alverez
+end
+
+def teacher_maria_jose_montiel
 end
 
 def test
 end
 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,11 @@ Rails.application.routes.draw do
   get '/pricing', to:  'pages#pricing'
   get '/contact', to:   'pages#contact'
   get '/join', to:  'pages#join'
+  get '/join', to: 'pages#help'
+  get '/enterprise', to: 'pages#enterprise'
+  get '/teacher-jorge-carrasco', to: 'pages#teacher_jorge_carrasco'
+  get '/teacher-mya-alverez', to: 'pages#teacher_jorge_carrasco'
+  get '/teacher-maria-jose-montiel', to: 'pages#teacher_jorge_carrasco'
   get '/test', to:   'pages#test'
 
 
