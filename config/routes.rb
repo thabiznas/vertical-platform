f91e31fa37991d16cb05aa635eebcde4441efc3c
Hunter Stevens <hunter@toplobster.io>
2019-02-26 00:27:15 +0000

    Create errors controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,12 @@ Rails.application.routes.draw do
   get 'spanish-teacher/maria-jose-montiel', to: 'pages#teacher_maria_jose_montiel'
   get '/test', to:   'pages#test'
   get '/test-2', to:   'pages#test_2'
+  
+  
+  get '/404', to:   'errors#not_found'
+  get '/422', to:   'errors#unacceptable'
+  get '/500', to:   'errors#internal_error'
+  
 
 
 
