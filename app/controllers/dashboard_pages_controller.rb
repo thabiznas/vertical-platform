b44380c19ae74de11d5d179196585e3a02391f2a
Hunter Stevens <hunter@toplobster.io>
2019-02-26 04:32:23 +0000

    Add responsiveness for tablets

---
+++ app/controllers/dashboard_pages_controller.rb
@@ -0,0 +1,2 @@
+class DashboardPagesController < ApplicationController
+end
---
+++ test/controllers/dashboard_pages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DashboardPagesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
