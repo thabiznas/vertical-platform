bba5dbf305d6ecafb4b04046675a1cfef144bf05
Hunter Stevens <hunter@toplobster.io>
2019-03-05 16:58:37 +0000

    Break down menu by view with extra authentication

--- app/controllers/dashboard_pages_controller.rb
+++ app/controllers/dashboard_pages_controller.rb
@@ -2,8 +2,7 @@ class DashboardPagesController < ApplicationController
   before_action :check_admin, only: [:admin]
   before_action :check_teacher, only: [:teacher]
   before_action :check_student, only: [:student]
-  layout 'dashboard'
-  
+    layout 'dashboard'
   
   def student
   end
