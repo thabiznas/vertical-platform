219eb7ddc7490103d4a78664ae47d3b1d5768edb
Hunter Stevens <hunter@toplobster.io>
2019-04-16 04:47:33 +0000

    Implement sign up flow redirects

--- app/controllers/dashboard_pages_controller.rb
+++ app/controllers/dashboard_pages_controller.rb
@@ -2,6 +2,8 @@ class DashboardPagesController < ApplicationController
   before_action :check_admin, only: [:admin]
   before_action :check_teacher, only: [:teacher]
   before_action :check_student, only: [:student]
+  before_action :check_subscription
+  
 
     layout 'dashboard'
   
@@ -65,10 +67,16 @@ class DashboardPagesController < ApplicationController
   end
   
     def check_student
-    if !current_user
+      if !current_user
       redirect_to :new_user_registration
+      end
     end
-  end
+
   
+    def check_subscription
+      if !current_user.subscribed 
+      redirect_to :plans
+      end
+    end
     
 end
